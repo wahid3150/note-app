a33c1fa8ceca3ae3fc9c945ee79ec01ca417d499
Wahid ali <wahidali3150@gmail.com>
2025-12-29 23:16:20 +0500

    Registration schema validation are added & tested working fine

---
+++ backend/validators/auth/registerSchema.js
@@ -0,0 +1,36 @@
+// 🧠 Golden Rule (Backend) api validation
+// If a route accepts any external input (body, params, query) → it MUST be validated.
+// That includes:
+// req.body
+// req.params
+// req.query
+
+import Joi from "joi";
+
+export const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(30).required().messages({
+    "string.base": "Username must be string",
+    "string.empty": "Username is required",
+    "string.min": "User name must be at least 3 character long",
+    "string.max": "Username must not exceed 30 characters",
+    "string.alphanum": "Username can only contain letters and numbers",
+    "any.required": "Username is required",
+  }),
+
+  email: Joi.string().email().required().messages({
+    "string.base": "Email must be a string",
+    "string.empty": "Email must be required",
+    "string.email": "Please provide a valid email address",
+    "any.required": "Email is required",
+  }),
+
+  password: Joi.string().min(8).max(128).required().messages({
+    "string.base": "Password must be a string",
+    "string.empty": "Password must be required",
+    "string.min": "Password must be at least 8 character long",
+    "string.max": "Password must not be exceed 128 characters",
+  }),
+}).options({
+  abortEarly: false, //Return all validation errors, not just the first
+  stripUnknown: true, //Remove fields not defined in the schema
+});
