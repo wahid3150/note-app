a6b631ec8bd1c65da146dfde06ee8fb381332844
Wahid ali <wahidali3150@gmail.com>
2025-12-30 14:11:13 +0500

    Cors package are setuped to allow the comming request form client side

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,17 @@ import "dotenv/config";
 import express from "express";
 import connectDB from "./config/db.js";
 import userRoute from "./routes/userRoute.js";
+import cors from "cors";
 
 const app = express();
 connectDB();
 const PORT = process.env.PORT || 3000;
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/auth", userRoute);
