b7a16a10f67211dd1692028fad362fdb14989cfb
Wahid ali <wahidali3150@gmail.com>
2025-12-31 18:39:45 +0500

    Email token verification page are created

---
+++ frontend/src/pages/Verify.jsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const Verify = () => {
+  const { token } = useParams();
+  const [status, setStatus] = useState("Verifying...");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const VerifyEmail = async () => {
+      try {
+        const res = await axios.post(
+          "http://localhost:8080/auth/verify",
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        if (res.data.success) {
+          setStatus("✅ Email Verified Successfully");
+          setTimeout(() => {
+            navigate("/login");
+          }, 2000);
+        } else {
+          setStatus("❌ Invalid token or expired");
+        }
+      } catch (error) {
+        console.log(error);
+        setStatus("❌ Verification failed, please try again");
+      }
+    };
+    VerifyEmail();
+  }, [token, navigate]);
+  return (
+    <div className="relative w-full h-190 bg-green-100 overflow-hidden">
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="bg-white p-6 rounded-xl shadow-md text-center w-[90%] max-w-md">
+          <h2 className="text-xl font-semibold text-gray-800">{status}</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Verify;
